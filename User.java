620e2ab7beb12b6bc74e843637e1dba22ca035bf
Neelo Mimi Pilane <cse24-116@thuto.bac.ac.bw>
2025-09-19 06:00:00 +0000

    Restore OOAD Part A Word document with original submission date

---
+++ User.java
@@ -0,0 +1,18 @@
+public abstract class User {
+    private String id;
+    private String name;
+    private String address;
+
+    public User(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+
+    public void setName(String name) { this.name = name; }
+    public void setAddress(String address) { this.address = address; }
+}
